15ad6571f02c264962d1720803ef28fdf39c57e6
Bjorn Jeurgens <bertus.men@gmail.com>
2023-08-16 20:01:09 +0200

    init

---
+++ spotronome/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Tempothing</h1>
+    <div class="nav-wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+      <a>Logout</a>
+    </div>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
